46eea3a57df1ef8ee1076e5672006a4929f0dfa9
KaikSouza <kaiksouza2020@gmail.com>
2022-05-16 14:26:40 -0300

    Conexão com o banco de dados

--- src/conexao/conexao.php
+++ src/conexao/conexao.php
@@ -2,9 +2,18 @@
 
 //CARREGAR AS CREDENCIAIS DO BANCO DE DADOS
 
-$hostname = "";
-$db = "";
-$username = "";
-$password = "";
+$hostname = "sql102.epizy.com";
+$database = "epiz_31478113_sistemarifas";
+$username = "epiz_31478113";
+$password = "5Bp4bR0QfI";
+
+try{
+    $pdo = new PDO('mysql:host='.$hostname.';dbname='.$database, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo 'Conexão com o banco de dados '.$database. 'foi realizada com sucesso!';
+}
+catch{
+
+}
 
 ?>
