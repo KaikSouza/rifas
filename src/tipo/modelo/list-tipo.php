e1c0625611b97ca4c763626287f3b6924dc3576a
KaikSouza <kaiksouza2020@gmail.com>
2022-06-06 14:15:02 -0300

    listagem

--- src/tipo/modelo/list-tipo.php
+++ src/tipo/modelo/list-tipo.php
@@ -18,3 +18,38 @@ $qtddeLinhas = $resultado->rowCount();
 
 //Verificar se existe algum filtro determinado pelo usuário
 $filtro = $requestData['search']['value'];
+if(!empty($filtro)){
+    //Montar a expressão lógico em sql para filtrar a nossa tabela
+    $sql .= " AND (ID LIKE '$filtro%' ";
+    $sql .= " OR NOME LIKE '$filtro%') ";
+}
+
+//Obter o total de registros exixstentes na tabela do banco de dados de acordo com o filtro
+$resultado = $pdo->query($sql);
+$totalFiltrados = $resultado->rowCount();
+
+//Obter o valores para ordenação
+$colunaOrdem = $requestData['order'][0]['column'];
+$ordem = $colunas[$colunaOrdem]['data'];
+$direcao = $requestData['order'][0]['dir'];
+
+//Obter os valores para o LIMIT
+$inicio = $requestData['start'];
+$tamanho = $requestData['length'];
+
+//Gerando a nossa ordenação na consulta sql
+$sql .= "ORDER BY $ordem $direcao LIMIT $inicio, $tamanho";
+$dados = array();
+while($row = $resultado->fetch(PDO::FETCH_ASSOC)){
+    $dados[] = array_map('utf8_encode', $row);
+}
+
+//Construir o nosso objeto json no padrão data table
+$json_data = array(
+    "draw" => intval($requestData['draw']),
+    "recordsTotal" => intval($qtddeLinhas),
+    "recordsFiltered" => intval($totalFiltrados),
+    "data" => $dados
+);
+
+echo json_encode($json_data);
