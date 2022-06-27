fcce94d7eb7dda09cacbf0230b4c7e119c71cf22
KaikSouza <kaiksouza2020@gmail.com>
2022-06-27 13:56:44 -0300

    alteração

---
+++ src/tipo/controle/delete-tipo.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+    $('#table-tipo').on('click', 'button.btn-excluir', function(e){
+        e.preventDefault()
+
+        let ID = `ID=${$(this).attr('id')}`
+
+        Swal.fire({
+            title: 'Gerenciamento de Rifas',
+            text: 'Você tem certeza que deseja excluir este registro?',
+            icon: 'question',
+            showCancelButton: true,
+            confirmButtonText: 'Sim',
+            cancelButtonText: 'Não'
+        }).then((result)=>{
+            if(result.value){
+                $.ajax({
+                    type: 'POST',
+                    dataType: 'JSON',
+                    assync: true,
+                    data: ID,
+                    url: 'src/tipo/modelo/delete-tipo.php',
+                    success: function(dados){
+                            Swal.fire({
+                                title: 'Gerenciamento de Rifas',
+                                text: dados.mensagem,
+                                icon: dados.tipo,
+                                confirmButtonText: 'OK'
+                            })
+                            $('#table-tipo').DataTable().ajax.reload()
+                        }
+                 })
+            }
+        })
+    })
+})
